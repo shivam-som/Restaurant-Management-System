fc1efcf1e86be00592cbdb9ed77ae30c70511164
shivam-som <thakurashu716@gmail.com>
2022-09-06 00:13:11 +0530

    initial commit for backend + frontend integration
    initial commit for backend + frontend integration

--- rms/src/App.js
+++ rms/src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Signup from './components/Signup';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
+
+const App = () => {
   return (
-    <div className="App">
-      Hello from rms !!
-    </div>
-  );
+    <>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup/new" element={<Signup />} />
+        </Routes>
+    </>
+  )
 }
 
 export default App;
